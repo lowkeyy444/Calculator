89fd024ae510ae928c49199ac3f1935451635c3e
lowkeyy444 <143279097+lowkeyy444@users.noreply.github.com>
2024-07-10 17:57:09 +0530

    created a basic calulator layout

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1,num2){
-    return num1+num2;
-}
-
+        return num1+num2;}
+    
 function sub(num1,num2){
-    return num1-num2;
-}
-
+        return num1-num2;}
+        
 function multiply(num1,num2){
-    return num1*num2;
-}
-
+        return num1*num2;}
+    
+      
 function divide(num1,num2){
-    return num1/num2;
+        return num1/num2;}
+    
+    
+    
+
+function operate(num1,operator,num2){
+    if(operator==='+'){return add(num1,num2);}
+    else  if(operator=='-'){return sub(num1,num2);}
+    else if(operator=='*'){return multiply(num1,num2);}
+    else if(operator=='/'){return divide(num1,num2);}
 }
 
-alert(divide(2,4))
+
+console.log(operate(1,'+',44));
