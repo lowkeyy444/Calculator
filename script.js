4cf681c82b090eb53c4bbdbd4f63d866d0ccb2f9
lowkeyy444 <143279097+lowkeyy444@users.noreply.github.com>
2024-07-11 12:30:33 +0530

    added equal to function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let num1;
-let num2;
+let num1 ='';
+let num2 ='';
 let operator;
 let numCount = 0;
 let numLimit = 15;
+let first = false;
 
 function add(num1,num2){
         return num1+num2;}
@@ -21,10 +22,12 @@ function divide(num1,num2){
     
 
 function operate(num1,operator,num2){
+    num1 =parseFloat(num1);
+    num2 = parseFloat(num2);
     if(operator==='+'){return add(num1,num2);}
     else  if(operator=='-'){return sub(num1,num2);}
-    else if(operator=='*'){return multiply(num1,num2);}
-    else if(operator=='/'){return divide(num1,num2);}
+    else if(operator=='×'){return multiply(num1,num2);}
+    else if(operator=='÷'){return divide(num1,num2);}
 }
 
 const btn = document.querySelectorAll("button");
@@ -38,33 +41,46 @@ btn.forEach(function(btns) {
    
       if(reg.test(buttonValue)){
         if(numCount<=numLimit){
-       const display = document.querySelector(".display");
-        const newNum = document.createElement("span");
-        newNum.textContent = buttonValue;
-       display.appendChild(newNum);
-       numCount++; 
-        }
-     if (buttonValue === '+') {
-        operator = "+";
-        num1 = newNum.value;
-        console.log(num1);
-    } else if (buttonValue === '-') {
-        operator = "-";
-        num1 = newNum.value;
-        console.log(num1);
-    } else if (buttonValue === '×') {
-        operator = "×";
-        num1 = newNum.value;
-        console.log(num1);
-    } else if (buttonValue === '÷') {
-        operator = "÷";
-        num1 = newNum.value;
-        console.log(num1);
-    }
+            const display = document.querySelector(".display");
+            const newNum = document.createElement("span");
+            newNum.textContent = buttonValue;
+            display.appendChild(newNum);
+            numCount++; 
 
-}   
+       if(first === false ){
+        num1 = num1 +buttonValue;
+       }else{
+        num2 = num2+buttonValue;
+       }
+    }
+}
+
+
+     else if(buttonValue === '+' || buttonValue === '-' || buttonValue === '×' || buttonValue === '÷') {
+        if (first === false) {
+            first = true;
+            operator = buttonValue;
+            console.log(operator);
+        }
+
+}
+    else if(buttonValue === '='){
+        if(num1 && operator&&num2){
+            const result = operate(num1,operator,num2);
+            const display = document.querySelector(".display");
+            display.textContent = result;
+            num1 = result;
+            num2 = '';
+            operator=null;
+            first=false;
+            numCount=0;
+
+        }
+    
+}
+
     });
     
 });
 
-operate(num1,operator,num2);
+// operate(num1,operator,num2);
